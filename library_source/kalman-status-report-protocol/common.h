408619f6bf0c77c2b319b89341708cae56231de7
Łukasz Kwinta <lukaszmind@gmail.com>
2024-07-31 01:11:30 +0200

    implement getters and setters, pack, health check getters

--- library_source/kalman-status-report-protocol/common.h
+++ library_source/kalman-status-report-protocol/common.h
@@ -5,10 +5,15 @@
 
 #define KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES 2
 
+#define _packed_ __attribute__((packed))
+#define _nonnull_ __attribute__((nonnull))
+
 typedef enum {
     OK,
     WARNING,
     CRITICAL,
+
+    UNKNOWN
 } StatusReportResult_TypeDef;
 
 #endif // KALMAN_PROTOCOL_STATUS_REPORT_COMMON_H_
--- out/wheels_protocol.h
+++ out/wheels_protocol.h
@@ -12,7 +12,7 @@
 #define KALMAN_STATUS_REPORT_PROTOCOL_SUBSYSTEM_ID 1
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// WheelsStatus Frame
+/// WheelsStatus Frame
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #define KALMAN_STATUS_REPORT_WHEELSSTATUS_ID 12
@@ -31,6 +31,12 @@ typedef enum {
     KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_CRITICAL_3_MAX = 300,
 } WheelsStatus_Temperature_HealthCheck_TypeDef;
 
+#define KALMAN_STATUS_WHEELSSTATUS_DRIVER_STATUS_HEALTH_CHECK_OK_1_DESCRIPTION "Driver is ready"
+#define KALMAN_STATUS_WHEELSSTATUS_DRIVER_STATUS_HEALTH_CHECK_CRITICAL_2_DESCRIPTION "Driver is not ready"
+#define KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_OK_1_DESCRIPTION "Temperature is normal"
+#define KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_WARNING_2_DESCRIPTION "Temperature is high"
+#define KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_CRITICAL_3_DESCRIPTION "Temperature is very high"
+
 typedef enum {
     ALGORITHM_TYPE_POSITION,
     ALGORITHM_TYPE_VELOCITY,
@@ -43,31 +49,169 @@ typedef enum {
     ALGORITHM_TYPE2_TORQUE = 3,
 } AlgorithmType2_TypeDef;
 
-typedef struct __attribute__((packed)) {
+typedef struct _packed_ {
     uint8_t controller_id;
     uint8_t driver_status;
     float temperature;
-    AlgorithmType_TypeDef algorithm_type;
-    AlgorithmType2_TypeDef algorithm_type2;
-    bool testbool;
+    uint8_t algorithm_type;
+    uint8_t algorithm_type2;
+    uint8_t testbool;
 } WheelsStatus_FrameTypeDef;
+
+////////////////////////////////////////
+/// Setters
+////////////////////////////////////////
+_nonnull_
+void WheelsStatus_set_controller_id(WheelsStatus_FrameTypeDef* frame, uint8_t value) {
+    frame->controller_id = value;
+}
+_nonnull_
+void WheelsStatus_set_driver_status(WheelsStatus_FrameTypeDef* frame, uint8_t value) {
+    frame->driver_status = value;
+}
+_nonnull_
+void WheelsStatus_set_temperature(WheelsStatus_FrameTypeDef* frame, float value) {
+    frame->temperature = value;
+}
+_nonnull_
+void WheelsStatus_set_algorithm_type(WheelsStatus_FrameTypeDef* frame, AlgorithmType_TypeDef value) {
+    frame->algorithm_type = (uint8_t)value;
+}
+_nonnull_
+void WheelsStatus_set_algorithm_type2(WheelsStatus_FrameTypeDef* frame, AlgorithmType2_TypeDef value) {
+    frame->algorithm_type2 = (uint8_t)value;
+}
+_nonnull_
+void WheelsStatus_set_testbool(WheelsStatus_FrameTypeDef* frame, bool value) {
+    frame->testbool = (uint8_t)value;
+}
+
+////////////////////////////////////////
+/// Getters
+////////////////////////////////////////
+_nonnull_
+uint8_t WheelsStatus_get_controller_id(const WheelsStatus_FrameTypeDef* frame) {
+    return frame->controller_id;
+}
+_nonnull_
+uint8_t WheelsStatus_get_driver_status(const WheelsStatus_FrameTypeDef* frame) {
+    return frame->driver_status;
+}
+_nonnull_
+float WheelsStatus_get_temperature(const WheelsStatus_FrameTypeDef* frame) {
+    return frame->temperature;
+}
+_nonnull_
+AlgorithmType_TypeDef WheelsStatus_get_algorithm_type(const WheelsStatus_FrameTypeDef* frame) {
+    return (AlgorithmType_TypeDef)frame->algorithm_type;
+}
+_nonnull_
+AlgorithmType2_TypeDef WheelsStatus_get_algorithm_type2(const WheelsStatus_FrameTypeDef* frame) {
+    return (AlgorithmType2_TypeDef)frame->algorithm_type2;
+}
+_nonnull_
+bool WheelsStatus_get_testbool(const WheelsStatus_FrameTypeDef* frame) {
+    return (bool)frame->testbool;
+}
 
 KalmanProtocol_StatusTypeDef WheelsStatus_unpack(const RawDataFrame_TypeDef* raw_data, WheelsStatus_FrameTypeDef* frame) {
     if (raw_data->length != sizeof(WheelsStatus_FrameTypeDef) + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES) {
-        return KALMAN_PROTOCOL_STATUS_INVALID_FRAME_SIZE;
+        return KALMAN_PROTOCOL_STATUS_INVALID_DATA_SIZE;
     }
 
     if (raw_data->data[0] != KALMAN_STATUS_REPORT_PROTOCOL_SUBSYSTEM_ID || raw_data->data[1] != KALMAN_STATUS_REPORT_WHEELSSTATUS_ID) {
         return KALMAN_PROTOCOL_STATUS_INVALID_FRAME_TYPE;
     }
-    frame->controller_id = *((uint8_t*)&raw_data->data[2]);
-    frame->driver_status = *((uint8_t*)&raw_data->data[3]);
-    frame->temperature = *((float*)&raw_data->data[4]);
-    frame->algorithm_type = (AlgorithmType_TypeDef)raw_data->data[5];
-    frame->algorithm_type2 = (AlgorithmType2_TypeDef)raw_data->data[6];
-    frame->testbool = *((bool*)&raw_data->data[7]);
+    
+    frame->controller_id = raw_data->data[0 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES];
+    frame->driver_status = raw_data->data[1 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES];
+    frame->temperature = *((float*)&raw_data->data[2 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES]);
+    frame->algorithm_type = raw_data->data[6 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES];
+    frame->algorithm_type2 = raw_data->data[7 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES];
+    frame->testbool = raw_data->data[8 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES];
 
     return KALMAN_PROTOCOL_STATUS_OK;
 }
+
+KalmanProtocol_StatusTypeDef WheelsStatus_pack(const WheelsStatus_FrameTypeDef* frame, RawDataFrame_TypeDef* raw_data) {
+    if (raw_data->capacity < sizeof(WheelsStatus_FrameTypeDef) + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES) {
+        return KALMAN_PROTOCOL_STATUS_INVALID_DATA_SIZE;
+    }
+
+    raw_data->data[0] = KALMAN_STATUS_REPORT_PROTOCOL_SUBSYSTEM_ID;
+    raw_data->data[1] = KALMAN_STATUS_REPORT_WHEELSSTATUS_ID;
+    
+    raw_data->data[0 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES] = frame->controller_id;
+    raw_data->data[1 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES] = frame->driver_status;
+    *((float*)&raw_data->data[2 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES]) = frame->temperature;
+    raw_data->data[6 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES] = frame->algorithm_type;
+    raw_data->data[7 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES] = frame->algorithm_type2;
+    raw_data->data[8 + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES] = frame->testbool;
+
+    raw_data->length = sizeof(WheelsStatus_FrameTypeDef) + KALMAN_PROTOCOL_STATUS_REPORT_ID_BYTES;
+
+    return KALMAN_PROTOCOL_STATUS_OK;
+}
+
+////////////////////////////////////////
+/// Health Checks
+////////////////////////////////////////
+StatusReportResult_TypeDef WheelsStatus_DriverStatus_HealthCheckResult(const WheelsStatus_FrameTypeDef* frame) {
+    if (frame->driver_status == KALMAN_STATUS_WHEELSSTATUS_DRIVER_STATUS_HEALTH_CHECK_OK_1) {
+        return OK;
+    }
+    if (frame->driver_status == KALMAN_STATUS_WHEELSSTATUS_DRIVER_STATUS_HEALTH_CHECK_CRITICAL_2) {
+        return CRITICAL;
+    }
+    return UNKNOWN;
+}
+
+StatusReportResult_TypeDef WheelsStatus_Temperature_HealthCheckResult(const WheelsStatus_FrameTypeDef* frame) {
+    if (frame->temperature >= KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_OK_1_MIN
+            && frame->temperature < KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_OK_1_MAX) {
+        return OK;
+    }
+    if (frame->temperature >= KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_WARNING_2_MIN
+            && frame->temperature < KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_WARNING_2_MAX) {
+        return WARNING;
+    }
+    if (frame->temperature >= KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_CRITICAL_3_MIN
+            && frame->temperature < KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_CRITICAL_3_MAX) {
+        return CRITICAL;
+    }
+    return UNKNOWN;
+}
+
+const char* WheelsStatus_DriverStatus_HealthCheckTroubleshoot(const WheelsStatus_FrameTypeDef* frame) {
+    return "Unknown troubleshoot";
+}
+
+const char* WheelsStatus_Temperature_HealthCheckTroubleshoot(const WheelsStatus_FrameTypeDef* frame) {
+    return "Unknown troubleshoot";
+}
+
+const char* WheelsStatus_DriverStatus_HealthCheckDescription(const WheelsStatus_FrameTypeDef* frame) {
+    if (frame->driver_status == KALMAN_STATUS_WHEELSSTATUS_DRIVER_STATUS_HEALTH_CHECK_OK_1) {
+        return KALMAN_STATUS_WHEELSSTATUS_DRIVER_STATUS_HEALTH_CHECK_OK_1_DESCRIPTION;
+    }
+    if (frame->driver_status == KALMAN_STATUS_WHEELSSTATUS_DRIVER_STATUS_HEALTH_CHECK_CRITICAL_2) {
+        return KALMAN_STATUS_WHEELSSTATUS_DRIVER_STATUS_HEALTH_CHECK_CRITICAL_2_DESCRIPTION;
+    }
+    return "Unknown description";
+}
+
+const char* WheelsStatus_Temperature_HealthCheckDescription(const WheelsStatus_FrameTypeDef* frame) {
+    if (frame->temperature == KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_OK_1) {
+        return KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_OK_1_DESCRIPTION;
+    }
+    if (frame->temperature == KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_WARNING_2) {
+        return KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_WARNING_2_DESCRIPTION;
+    }
+    if (frame->temperature == KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_CRITICAL_3) {
+        return KALMAN_STATUS_WHEELSSTATUS_TEMPERATURE_HEALTH_CHECK_CRITICAL_3_DESCRIPTION;
+    }
+    return "Unknown description";
+}
+
 
 #endif // KALMAN_STATUS_REPORT__H_
